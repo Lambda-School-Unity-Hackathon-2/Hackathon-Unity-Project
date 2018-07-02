336dc422a1c5eeab3bade54f4a9be2e4f1462a26
bri79319 <bri79319@gmail.com>
2018-07-02 13:57:23 -0400

    Made missile projection

---
+++ Assets/Player Design/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public GameObject misslePrefab;
+    public Transform missleSpawn;
+    public float missleSpeed = 40f;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Fire();
+        }
+    }
+
+
+    void Fire()
+    {
+        // Create the missle from the missle Prefab
+        var missle = (GameObject)Instantiate(
+            misslePrefab,
+            missleSpawn.position,
+            missleSpawn.rotation);
+
+        // Add velocity to the missle
+        missle.GetComponent<Rigidbody>().velocity = missle.transform.forward * missleSpeed;
+
+        // Destroy the missle after 2 seconds
+        Destroy(missle, 2.0f);
+    }
+}
