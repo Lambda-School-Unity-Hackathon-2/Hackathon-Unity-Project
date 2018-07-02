ffaec1737326717693cd2cb60176923d328a28b8
Charles Cortinas <crcortinas@gmail.com>
2018-07-02 14:15:33 -0500

    Enemy AI
    Renamed patrol  to EnemyController, added player tracking and health to enemies.

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -3,18 +3,20 @@ using UnityEngine.AI;
 using System.Collections;
 
 
-public class Patrol : MonoBehaviour {
+public class EnemyController : MonoBehaviour {
 
     public Transform[] points;
+		public Transform player;
+		public int startingHealth = 100;
+		private int currentHealth;
     private int destPoint = 0;
     private NavMeshAgent agent;
 
 
     void Start () {
         agent = GetComponent<NavMeshAgent>();
-
         agent.autoBraking = false;
-
+				currentHealth = startingHealth;
         GotoNextPoint();
     }
 
@@ -30,7 +32,18 @@ public class Patrol : MonoBehaviour {
 
 
     void Update () {
+			if(currentHealth <= 0){
+				carDeath();
+			}
+			if(Vector3.Distance(transform.position, player.position) > 40){
         if (!agent.pathPending && agent.remainingDistance < 0.5f)
             GotoNextPoint();
-    }
+			} else {
+				agent.destination = player.position;
+			}
+		}
+
+		void carDeath() {
+			gameObject.SetActive(false);
+		}
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,23 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public float speed;
+
+	private Rigidbody rb;
+
+	void Start() {
+		rb = GetComponent<Rigidbody>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizontal,0,moveVertical);
+
+		rb.AddForce(movement * speed);
 	}
 }
